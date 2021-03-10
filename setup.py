ac2d07a83c0874906dba8520f0977168804690af
jungr-ait <roland.jung@aau.at>
2021-03-10 11:17:30 +0100

    [TASK] setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='spatial_csv_format',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Roland Jung',
+      author_email='roland.jung@aau.at',
+      url='https://gitlab.aau.at/aau-cns/py3_pkgs/spatial_csv_format/',
+      packages=['distutils', 'distutils.command'],
+     )
